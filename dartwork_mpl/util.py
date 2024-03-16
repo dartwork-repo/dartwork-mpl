76b1a34c45e5ab0582f59d76629a4dd72cc872da
Sangwon91 <lsw91.main@gmail.com>
2024-03-16 15:57:09 +0900

    Add plot_colors function to util.py

--- dartwork_mpl/util.py
+++ dartwork_mpl/util.py
@@ -1,3 +1,4 @@
+import math
 from xml.dom import minidom
 from tempfile import NamedTemporaryFile
 from pathlib import Path
@@ -7,9 +8,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
+from matplotlib.patches import Rectangle
 from matplotlib.transforms import ScaledTranslation
 from IPython.display import display, HTML, SVG
 from scipy.optimize import minimize
+
 
 # Will be replaced to rich print.
 PRINT = print
@@ -311,3 +314,58 @@ def plot_colormaps(cmap_list=None):
         ax.set_axis_off()
 
     return fig, axs
+
+
+def plot_colors(colors=None, *, ncols=4, sort_colors=True):
+    if colors is None:
+        colors = {
+            k: v for k, v in mcolors.get_named_colors_mapping().items()
+            if not k.startswith('dartwork_mpl.')
+        }
+
+    cell_width = 212
+    cell_height = 22
+    swatch_width = 48
+    margin = 12
+
+    # Sort colors by hue, saturation, value and name.
+    if sort_colors is True:
+        names = sorted(
+            colors, key=lambda c: tuple(mcolors.rgb_to_hsv(mcolors.to_rgb(c))))
+    else:
+        names = list(colors)
+
+    n = len(names)
+    nrows = math.ceil(n / ncols)
+
+    width = cell_width * ncols + 2 * margin
+    height = cell_height * nrows + 2 * margin
+    dpi = 72
+
+    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
+    fig.subplots_adjust(margin/width, margin/height,
+                        (width-margin)/width, (height-margin)/height)
+    ax.set_xlim(0, cell_width * ncols)
+    ax.set_ylim(cell_height * (nrows-0.5), -cell_height/2.)
+    ax.yaxis.set_visible(False)
+    ax.xaxis.set_visible(False)
+    ax.set_axis_off()
+
+    for i, name in enumerate(names):
+        row = i % nrows
+        col = i // nrows
+        y = row * cell_height
+
+        swatch_start_x = cell_width * col
+        text_pos_x = cell_width * col + swatch_width + 7
+
+        ax.text(text_pos_x, y, name, fontsize=14,
+                horizontalalignment='left',
+                verticalalignment='center')
+
+        ax.add_patch(
+            Rectangle(xy=(swatch_start_x, y-9), width=swatch_width,
+                      height=18, facecolor=colors[name], edgecolor='0.7')
+        )
+
+    return fig
