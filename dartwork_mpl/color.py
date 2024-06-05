05bddb0f201cee9d927fcb81c4b7b75d4d2fde2f
Sangwon91 <lsw91.main@gmail.com>
2024-06-05 20:55:47 +0900

    add tailwind colors

--- dartwork_mpl/color.py
+++ dartwork_mpl/color.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import matplotlib.colors as mcolors
 
@@ -33,6 +34,16 @@ def _load_colors():
     # Append prefix to distinguish them from matplotlib colors.
     _color_dict = {f'dm.{k}': v for k, v in color_dict.items()}
     _color_dict.update({f'dartwork_mpl.{k}': v for k, v in color_dict.items()})
+
+    # Tailwind colors.
+    with open(root_dir / 'tailwind_colors.json', 'r') as f:
+        tailwind_colors = json.load(f)
+
+    for k, v in tailwind_colors.items():
+        k = k.lower()
+        for weight, hex in v:
+            _color_dict[f'tailwind.{k}:{weight}'] = f'#{hex}'
+            _color_dict[f'tw.{k}:{weight}'] = f'#{hex}'
 
     # Add color dict to matplotlib internal color mapping.
     mcolors.get_named_colors_mapping().update(_color_dict)
