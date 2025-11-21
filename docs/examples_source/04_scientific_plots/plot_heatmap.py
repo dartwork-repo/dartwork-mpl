d2e88e5cdb0e56624a4bdcf28573be9ea0c890c7
Wonjun <wonjunn.choi@gmail.com>
2025-11-21 18:50:40 +0900

    docs: Adjust heatmap plot layout by modifying figure size and grid specifications for improved visual clarity and spacing between panels.

--- docs/examples_source/04_scientific_plots/plot_heatmap.py
+++ docs/examples_source/04_scientific_plots/plot_heatmap.py
@@ -18,13 +18,13 @@ cov = rng.normal(0, 1, size=(200, 8))
 cov[:, 4:] += cov[:, :4] * 0.6  # induce correlation structure
 corr = np.corrcoef(cov, rowvar=False)
 
-fig = plt.figure(figsize=(dm.cm2in(14), dm.cm2in(10)), dpi=300)
+fig = plt.figure(figsize=(dm.cm2in(18), dm.cm2in(9)), dpi=300)
 gs = fig.add_gridspec(
-    2, 1,
-    height_ratios=[1, 1],
-    left=0.10, right=0.85,
-    top=0.95, bottom=0.10,
-    hspace=0.32
+    1, 2,
+    width_ratios=[1, 1],
+    left=0.08, right=0.94,
+    top=0.92, bottom=0.12,
+    wspace=0.28
 )
 
 # Panel A: Diverging heatmap
@@ -37,7 +37,7 @@ ax1.set_xlabel('X Axis', fontsize=dm.fs(0))
 ax1.set_ylabel('Y Axis', fontsize=dm.fs(0))
 
 # Panel B: Correlation matrix
-ax2 = fig.add_subplot(gs[1, 0])
+ax2 = fig.add_subplot(gs[0, 1])
 im2 = ax2.imshow(corr, cmap='dm.Spectral', vmin=-1, vmax=1)
 cbar2 = fig.colorbar(im2, ax=ax2, fraction=0.04, pad=0.02)
 cbar2.set_label('Correlation', fontsize=dm.fs(0))
